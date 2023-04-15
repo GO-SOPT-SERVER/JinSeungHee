050f6ec855adabe0305fb0a4c5f7a09b1e5f6d91
Jin409 <sualng123@gmail.com>
2023-04-15 14:16:42 +0900

    2주차 심화 과제 초기 세팅

---
+++ assignments/second_week/secondAssignment/src/test/java/sopt/org/secondAssignment/SecondAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package sopt.org.secondAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
