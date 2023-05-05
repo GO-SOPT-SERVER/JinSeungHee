00461fb3c61b766ecd24393eb84ce243edd9b131
Jin409 <sualng123@gmail.com>
2023-05-06 02:37:05 +0900

    refactor: dto 단의 입력값이 null 인 경우 예외를 던지도록

--- assignments/third_week/src/main/java/assignment/ThirdWeek/post/service/dto/PostRegisterDto.java
+++ assignments/third_week/src/main/java/assignment/ThirdWeek/post/service/dto/PostRegisterDto.java
@@ -2,12 +2,16 @@ package assignment.ThirdWeek.post.service.dto;
 
 import assignment.ThirdWeek.post.domain.Post;
 import assignment.ThirdWeek.user.domain.User;
+import javax.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class PostRegisterDto {
+    @NotNull
     private Long userId;
+    @NotNull
     private String title;
+    @NotNull
     private String content;
 
     public Post toEntity(User user) {
--- assignments/third_week/src/main/java/assignment/ThirdWeek/user/service/dto/UserRegisterDto.java
+++ assignments/third_week/src/main/java/assignment/ThirdWeek/user/service/dto/UserRegisterDto.java
@@ -18,6 +18,7 @@ public class UserRegisterDto {
             regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}",
             message = "비밀번호는 영문 대,소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8자 ~ 20자의 비밀번호여야 합니다."
     )
+    @NotNull
     private final String password;
 
     public User toEntity() {
